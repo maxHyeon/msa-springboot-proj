423740e3c0dcf0151ea99496c83187d63ee9895c
minhyeon <parkmh90@kr.ibm.com>
2019-12-23 14:47:31 +0900

    Initial commit

---
+++ src/main/java/com/maxhyeon/msaspringbootproj/MsaSpringbootProjApplication.java
@@ -0,0 +1,13 @@
+package com.maxhyeon.msaspringbootproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaSpringbootProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaSpringbootProjApplication.class, args);
+	}
+
+}
